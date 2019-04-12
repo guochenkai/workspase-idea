250b392f7e38e577e034169fb2bab630910d56df
guochenkai <chenkaiwinner@163.com>
2019-04-12 20:26:27 +0800

    chenkaiWinner

---
+++ boot/src/main/java/com/jk/Controller/UserController.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: UserController
+ * Author:   郭晨凯
+ * Date:     2019-04-12 9:11
+ * Description: usercontroller
+ * History:
+ * 郭晨凯        <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.jk.Controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈usercontroller〉
+ *
+ * @author chenkai
+ * @create 2019-04-12
+ * @since 1.0.0
+ */
+
+@Controller
+public class UserController {
+}
